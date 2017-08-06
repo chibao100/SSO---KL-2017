72a4e75455271c4604ee384ec4fc2acbeba59ca9
giangthanhhai17@gmail.com <giangthanhhai17@gmail.com>
2017-08-06 17:38:15 +0700

    conflit

--- Tour_SSO/Controllers/HomeController.cs
+++ Tour_SSO/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Tour_SSO.Controllers
     {
         public ActionResult Index()
         {
-           
+            int dis = "me.";
             UnitOfWork _unitOfWork = new UnitOfWork(new TourEntities());
             ViewBag.InTour = _unitOfWork.TourRepo.GetAll().Where(x => x.InOutType == 1).Take(4).ToList();
             ViewBag.OutTour = _unitOfWork.TourRepo.GetAll().Where(x => x.InOutType == 2).Take(4).ToList();
